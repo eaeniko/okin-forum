1b5aad8e316adb9a2338dfd3b3bade48fc815305
Nikolai <eaenikolai@gmail.com>
2026-07-01 09:26:23 -0300

    add scripts `link:check and lint:write`

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return <h1 className="text-3xl font-bold underline">Under construction</h1>;
 }
 export default Home;
