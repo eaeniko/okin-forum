1425b850d5dba3c63a0494770b73033be42d4519
Nikolai <eaenikolai@gmail.com>
2026-07-08 13:38:20 -0300

    add `database.js` and `.env`files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,8 @@
-function status(request, response) {
-  response.status(200).json({ status: 'OK' });
+import database from "../../../../infra/database.js";
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum");
+  console.log(result);
+  response.status(200).json({ status: "OK" });
 }
 
 export default status;
